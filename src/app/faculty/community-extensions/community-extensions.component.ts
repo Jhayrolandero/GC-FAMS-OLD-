d0214aa9307c7eb2664210b9d790298b1623cd22
Cjay Landero <xjaylandero@gmail.com>
2024-02-26 10:26:13 +0800

    done styles

--- src/app/faculty/community-extensions/community-extensions.component.ts
+++ src/app/faculty/community-extensions/community-extensions.component.ts
@@ -2,23 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { CommunityExtensionService } from '../../services/faculty/community-extension.service';
 import { CommunityExtension } from '../../services/faculty/community-extension';
 import { OtherCommexComponent } from './other-commex/other-commex.component';
+import { NgFor, SlicePipe } from '@angular/common';
 @Component({
   selector: 'app-community-extensions',
   standalone: true,
-  imports: [OtherCommexComponent],
+  imports: [OtherCommexComponent, NgFor, SlicePipe],
   templateUrl: './community-extensions.component.html',
   styleUrl: './community-extensions.component.css'
 })
 export class CommunityExtensionsComponent implements OnInit{
-  commex: CommunityExtension[] = [];
-
+  commexs: CommunityExtension[] = [];
+  min: number = 100;
+  max: number = 250;
   constructor(private commService: CommunityExtensionService){}
 
   getCommex():void {
     this.commService.getAllCommex().subscribe(
-      commex => this.commex = commex
+      (commex :any) => {
+        this.commexs = commex
+      }
+
     )
   }
+
+  getRndInteger(): number {
+    return Math.floor(Math.random() * (this.max - this.min + 1) ) + this.min;
+  }
+
   ngOnInit(): void {
     this.getCommex()
   }

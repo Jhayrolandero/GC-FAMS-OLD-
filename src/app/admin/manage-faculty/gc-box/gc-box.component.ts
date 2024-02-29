025d9a640f618b075ee1e67b824f22ae37f272b6
Cjay Landero <xjaylandero@gmail.com>
2024-02-29 22:13:09 +0800

    added gc-box

--- src/app/admin/manage-faculty/gc-box/gc-box.component.ts
+++ src/app/admin/manage-faculty/gc-box/gc-box.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gc-box',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './gc-box.component.html',
   styleUrl: './gc-box.component.css'
 })
 export class GcBoxComponent {
 
+  @Input('collegeAbbv') collegeAbbv: string = ''
+  @Input('collegeTitle') collegeTitle: string = ''
+  @Input('imgPath') imgPath: string = ''
+  @Input('bgColor') bgColor: string = ''
 }
